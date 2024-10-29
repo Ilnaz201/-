0848f1d01aedcb76757cc8293ce74502a386483a
Ilnaz <sadykovilnaz096@gmail.com>
2024-10-30 02:28:01 +0500

    Практическая работа по уроку Динамическая (типизация исправленная версия от 30.10.24)

--- module_1_3.py
+++ module_1_3.py
@@ -1,7 +1,11 @@
 name = "ilnaz"
 print(name)
 age = 19
-print(age)
-now_age = 20
-print(now_age)
-print (True,("is_studen"))
+print (age)
+print (age + 1)
+next_year_age = 20
+print(next_year_age)
+is_student = True
+print(is_student)
+
+
