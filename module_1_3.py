cd4e742661e89503637ed05a46991d0358552acc
Ilnaz <sadykovilnaz096@gmail.com>
2024-11-14 17:51:48 +0500

    исправленная версия динамической типизации

--- module_1_3.py
+++ module_1_3.py
@@ -1,10 +1,9 @@
 name = "ilnaz"
 print(name)
 age = 19
-print (age)
-print (age + 1)
-next_year_age = 20
-print(next_year_age)
+print(age)
+age = age + 1
+print(age)
 is_student = True
 print(is_student)
 
